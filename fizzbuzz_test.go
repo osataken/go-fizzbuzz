beeaefecba50315df8c08e5075dae2b2f1452626
Kendo <osataken@gmail.com>
2015-07-02 11:34:15 +0700

    add second fail test

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -13,3 +13,13 @@ func Test_input_1_should_return_1(t *testing.T) {
 		t.Errorf("Expected %V but was %V", expected, result)
 	}
 }
+
+func Test_input_3_should_return_fizz(t *testing.T) {
+	expected := "fizz"
+
+	result := fizzbuzz("3")
+
+	if result != expected {
+		t.Errorf("Expected %V but was %V", expected, result)
+	}
+}
